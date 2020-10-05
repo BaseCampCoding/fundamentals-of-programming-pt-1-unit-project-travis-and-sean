15677aa938c111cd087073c59f416c67df4bc119
Sean <72217270+seanc21@users.noreply.github.com>
2020-10-05 11:20:09 -0500

    Add files via upload

---
+++ random_game.py
@@ -0,0 +1,8 @@
+import random
+number = random.randint(1, 20)
+players_name = input(" What is your name? ")
+number_of_guesses = 0
+print(player_name + " I am Guessing a number between 1 and 20:")
+ 
+while number_of_guesses < 5:
+    player_number = 
