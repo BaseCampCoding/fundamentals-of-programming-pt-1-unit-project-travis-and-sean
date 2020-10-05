95b1fc58472be26956a7cef8322c8e224437b561
Travis-Alexander <72216997+Travis-Alexander@users.noreply.github.com>
2020-10-05 15:06:27 -0500

    Fixed life counter

--- random_game.py
+++ random_game.py
@@ -1,12 +1,12 @@
 import random
 number = random.randint(1, 20)
-player_name = input(" What is your name? ")
-number_of_guesses = 0
-print(player_name + " I am Guessing a number between 1 and 20:")
- 
-while number_of_guesses < 5:
+player_name = input("What is your name? \n")
+print(player_name + ", I am guessing a number between 1 and 20: ")
+lives = 5
+
+while lives > 0:
     player_number = input("What is your number? ")
-    number_of_guesses += 1
+    lives -= 1
     if player_number < number:
         print("Your guess is too low")
-    elif player_number 
+    
