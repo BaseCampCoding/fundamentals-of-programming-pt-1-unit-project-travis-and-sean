77cb4737a72a971049b20a031f7d9a761048715a
Travis-Alexander <72216997+Travis-Alexander@users.noreply.github.com>
2020-10-08 16:17:57 -0500

    added pick a card game

--- random_game.py
+++ random_game.py
@@ -147,7 +147,107 @@ def the_random_game():
                 result = "\nIt was Heads!"
                 print(Fore.LIGHTRED_EX + result + Style.RESET_ALL)
                 lives -= 1
-                print(Fore.LIGHTMAGENTA_EX + f"\nYou have {lives} lives left!" + Style.RESET_ALL)                
+                print(Fore.LIGHTMAGENTA_EX + f"\nYou have {lives} lives left!" + Style.RESET_ALL) 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    def cardPickup():
+        global lives
+        print("I have a deck of 52 cards not including the Jokers.\nI have selected a random card.")
+        cardChoice = input("What card is in my hand?\n").lower()
+        cards = ["ace of spades", "ace of hearts", "ace of diamonds", "ace of clubs",  "2 of spades", "2 of hearts", "2 of diamonds", "2 of clubs", "3 of spades", "3 of hearts", "3 of diamonds", "3 of clubs", "4 of spades", "4 of hearts", "4 of diamonds", "4 of clubs", "5 of spades", "5 of hearts", "5 of diamonds", "5 of clubs", "6 of spades", "6 of hearts", "6 of diamonds", "6 of clubs", "7 of spades", "7 of hearts", "7 of diamonds", "7 of clubs", "8 of spades", "8 of hearts", "8 of diamonds", "8 of clubs", "9 of spades", "9 of hearts", "9 of diamonds", "9 of clubs", "10 of spades", "10 of hearts", "10 of diamonds", "10 of clubs", "jack of spades", "jack of hearts", "jack of diamonds", "jack of clubs", "queen of spades", "queen of hearts", "queen of diamonds", "queen of clubs", "king of spades", "king of hearts", "king of diamonds", "king of clubs"]
+        while cardChoice not in cards:
+            print("Thats not a card...")
+            cardChoice = input("What card is in my hand?\n")
+            if cardChoice in cards:
+                break
+        wins = 0
+        drop = random.choice(cards)
+        while lives > 0 and wins < 1:
+            if cardChoice == drop:
+                print("That was my card!")
+                wins +=1
+                break
+            elif cardChoice != drop:
+                print("That is not my card.")
+                lives -= 1
+                print(f"You have {lives} lives left.")
+                if drop.endswith("spades"):
+                    print("Your card is a spade.")
+                elif drop.endswith("hearts"):
+                    print("Your card is a heart.")
+                elif drop.endswith("diamonds"):
+                    print("Your card is a diamond.")
+                elif drop.endswith("clubs"):
+                    print("Your card is a club.")
+                if lives == 0:
+                    print("You have ran out of lives!")
+                    break
+                cardChoice = input("What card is in my hand?\n").lower()
+                while cardChoice not in cards:
+                    print("Thats not a card...")
+                    cardChoice = input("What card is in my hand?\n")
+                    if cardChoice in cards:
+                        break               
         
     games = ["rock paper scissors", "guess the number", "coin flip"]
     
