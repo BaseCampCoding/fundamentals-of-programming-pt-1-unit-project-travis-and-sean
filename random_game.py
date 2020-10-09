2073dc476b41c9e3c4ec0d7749c5c3977f43cbeb
Sean Coh <scoh21@basecampcodingacademy.org>
2020-10-09 08:26:17 -0500

    merching conflicts

--- random_game.py
+++ random_game.py
@@ -216,7 +216,7 @@ def the_random_game():
         cardChoice = input("What card is in my hand?\n").lower()
         cards = ["ace of spades", "ace of hearts", "ace of diamonds", "ace of clubs",  "2 of spades", "2 of hearts", "2 of diamonds", "2 of clubs", "3 of spades", "3 of hearts", "3 of diamonds", "3 of clubs", "4 of spades", "4 of hearts", "4 of diamonds", "4 of clubs", "5 of spades", "5 of hearts", "5 of diamonds", "5 of clubs", "6 of spades", "6 of hearts", "6 of diamonds", "6 of clubs", "7 of spades", "7 of hearts", "7 of diamonds", "7 of clubs", "8 of spades", "8 of hearts", "8 of diamonds", "8 of clubs", "9 of spades", "9 of hearts", "9 of diamonds", "9 of clubs", "10 of spades", "10 of hearts", "10 of diamonds", "10 of clubs", "jack of spades", "jack of hearts", "jack of diamonds", "jack of clubs", "queen of spades", "queen of hearts", "queen of diamonds", "queen of clubs", "king of spades", "king of hearts", "king of diamonds", "king of clubs"]
         while cardChoice not in cards:
-            print(Fore.LIGHTMAGNETA_EX + "Thats not a card..." + Style.RESET_ALL)
+            print(Fore.LIGHTMAGENTA_EX + "Thats not a card..." + Style.RESET_ALL)
             cardChoice = input("What card is in my hand?\n")
             if cardChoice in cards:
                 break
@@ -224,7 +224,7 @@ def the_random_game():
         drop = random.choice(cards)
         while lives > 0 and wins < 1:
             if cardChoice == drop:
-                print( Fore.LIGHTGREEN_EX + "\nThat was my card!" Style.RESET_ALL)
+                print( Fore.LIGHTGREEN_EX + "\nThat was my card!" + Style.RESET_ALL)
                 wins +=1
                 break
             elif cardChoice != drop:
